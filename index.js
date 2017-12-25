c90783c3456f6e00378d8fa12a3205bde895bf24
Nicolas <32208373+nicoaugereau@users.noreply.github.com>
2017-12-25 19:38:11 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var shutdown = require('./lib/shutdown.js');
+var reboot = require('./lib/reboot.js');
+var restart = require('./lib/restart.js');
+var restartBluetooth = require('./lib/restartBluetooth.js');
+var restartVoice = reauire('./lib/restartVoice.js');
+var command = require('./lib/commands.command.js');
+var install = require('./lib/install.js');
+var uninstall = require('./lib/uninstall.js');
+
+module.exports = function(sails) {
+
+	return {
+		install: install,
+		uninstall: uninstall,
+		shutdown: shutdown,
+		reboot: reboot,
+		restart: restart,
+		restartBluetooth : restartBluetooth,
+		restartVoice : restartVoice,
+		command: command
+	};
+};
