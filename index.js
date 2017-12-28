5d237caffbe1b7db66b6b323980ae7eb75f86ef6
Nicolas <32208373+nicoaugereau@users.noreply.github.com>
2017-12-28 15:14:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 module.exports = function(sails) {
+	var install = require('./lib/install.js');
+	var uninstall = require('./lib/uninstall.js');
 	var shutdown = require('./lib/shutdown.js');
 	var reboot = require('./lib/reboot.js');
 	var restart = require('./lib/restart.js');
 	var restartBluetooth = require('./lib/restartBluetooth.js');
 	var restartVoice = require('./lib/restartVoice.js');
 	var command = require('./lib/commands.command.js');
-	var install = require('./lib/install.js');
-	var uninstall = require('./lib/uninstall.js');
 
 	return {
 		install: install,
